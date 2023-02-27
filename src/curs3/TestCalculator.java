278e0095b6d4450c89952acf1852dcdbf4b71131
Elena Albu <ealbu@KRO.LOCAL>
2023-02-27 21:45:09 +0200

    calculatorul din curs3

---
+++ src/curs3/TestCalculator.java
@@ -0,0 +1,13 @@
+package curs3;
+
+public class TestCalculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SimpleCalculator calc = new SimpleCalculator();
+		
+		calc.askTheUser();
+		calc.calculator();
+	}
+
+}
