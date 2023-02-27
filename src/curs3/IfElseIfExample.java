200753ecc70ed6e6016a92d9156a24cd56f15ffa
Elena Albu <ealbu@KRO.LOCAL>
2023-02-27 23:29:13 +0200

    actualizare IfElseIf

--- src/curs3/IfElseIfExample.java
+++ src/curs3/IfElseIfExample.java
@@ -1,32 +1,34 @@
 package curs3;
 import java.util.Scanner;
 public class IfElseIfExample  {
-	/**
-	 * * intrebam userul punctajul unui test si ii atribuim un calificativ
+	/** intrebam userul punctajul unui test si ii atribuim un calificativ
 	 *  a obtinut <=90 --> B
 	 *  a obtinut <=  80 --> C
 	 *  a obtinut <= 70 --> D
 	 *  a obtinut <= 60 --> E
 	 *  peste 90 --> A
 	 */
-public static void main(String[] args) {
+	public static void main(String[] args) 
+		{
 	
 		
-		Scanner scan = new Scanner(System.in);
-		
 		System.out.println("Ce punctaj ai obtinut?");
+		Scanner scan = new Scanner(System.in);
 		int punctaj = scan.nextInt();
 		char calificativ ;
 		
 		if(punctaj <= 60) { 
 			calificativ = 'E';
 			
-		}else if (punctaj <=70) {calificativ = 'D';
-		}else if (punctaj <=80) { calificativ = 'C';
-		}else if (punctaj <=90) { calificativ = 'B';
-		}else  calificativ = 'A';}
-System.out.println("Calificativul primit este: " + calificativ);
-					
+		}else if (punctaj <=70) {
+			calificativ = 'D';
+		}else if (punctaj <=80) { 
+			calificativ ='C';
+		}else if (punctaj <=90) { 
+			calificativ = 'B';
+		}else  calificativ = 'A';
+		System.out.println("Calificativul primit este: " + calificativ);
+		}		
 			
 	}
 
--- src/curs3/TestParSauImpar.java
+++ src/curs3/TestParSauImpar.java
@@ -5,7 +5,7 @@ public class TestParSauImpar {
 	public static void main(String[] args) {
 		
 		ParSauImpar obj = new ParSauImpar();
-		obj.verificaParSauImpar(8);
+		obj.verificaParSauImpar(9);
 	}
 
 }
