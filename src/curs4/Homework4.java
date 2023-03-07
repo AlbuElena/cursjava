862d058d8cf63b2083bb472b8127a9b6baf9c2af
Elena Albu <ealbu@KRO.LOCAL>
2023-03-07 12:01:59 +0200

    curs4 Homework4 cu metoda de print

--- src/curs4/Homework4.java
+++ src/curs4/Homework4.java
@@ -4,50 +4,54 @@ import java.util.Scanner;
 
 public class Homework4 {
 	
-
+	static String ziuaSaptamanii;
+	
+	public static void Printare() {
+		System.out.println("Ziua saptamanii este " + ziuaSaptamanii);
+	}
+	
 	public static void main(String[] args) {
 		// afiseaza ziua saptamanii corespunzatoare numarului introdus
 		
 		System.out.println("Introdu un numar:");
 		Scanner scan = new Scanner(System.in);
 		int a = scan.nextInt();
-		String ziuaSaptamanii = null;
-		
+				
 		switch (a) {
 		case 1: 
 			ziuaSaptamanii = "Luni";
-			//System.out.println("Ziua saptamanii este " + ziuaSaptamanii); 
+			Printare();
 			break;
 		case 2 : 
 			ziuaSaptamanii = "Marti";
-			//System.out.println("Ziua saptamanii este " + ziuaSaptamanii); 
+			Printare();
 			break;
 		case 3 : 
 			ziuaSaptamanii = "Miercuri";
-			//System.out.println("Ziua saptamanii este " + ziuaSaptamanii); 
+			Printare();
 			break;
 		case 4 : 
 			ziuaSaptamanii = "Joi";
-			//System.out.println("Ziua saptamanii este " + ziuaSaptamanii); 
+			Printare(); 
 			break;
 		case 5 : 
 			ziuaSaptamanii = "Vineri";
-			//System.out.println("Ziua saptamanii este " + ziuaSaptamanii); 
+			Printare();
 			break;
 		case 6 : 
 			ziuaSaptamanii = "Sambata";
-			//System.out.println("Ziua saptamanii este " + ziuaSaptamanii); 
+			Printare();
 			break;
 		case 7 : 
 			ziuaSaptamanii = "Duminica";
-			//System.out.println("Ziua saptamanii este " + ziuaSaptamanii); 
+			Printare();
 			break;
-		//default: System.out.println("Te rugam sa introduci un numar intre 1 si 7");
+		default: System.out.println("Te rugam sa introduci un numar intre 1 si 7");
 		
 		}
-	if(a<=7&&a>0) {
+	/*if(a<=7&&a>0) {
 		System.out.println("Ziua saptamanii este " + ziuaSaptamanii); 
-	}else System.out.println("Te rugam sa introduci un numar intre 1 si 7");
+	}else System.out.println("Te rugam sa introduci un numar intre 1 si 7");*/
 	}
 
 }
