160e5cef6cf9fe2963ccfa296a194ba948302fd5
Elena Albu <ealbu@KRO.LOCAL>
2023-03-13 19:06:31 +0200

    ceva

---
+++ src/curs6/TestEu1.java
@@ -0,0 +1,20 @@
+package curs6;
+
+public class TestEu1 {
+
+	public int varsta;
+	
+	public TestEu1(int x) {
+		//this.varsta = varsta;
+		this.varsta = x;
+	}
+
+	public int getVarsta() {
+		return varsta;
+	}
+
+	public void setVarsta(int varsta) {
+		this.varsta = varsta;
+	}
+
+}
---
+++ src/curs6/TestEu2.java
@@ -0,0 +1,14 @@
+package curs6;
+
+public class TestEu2 {
+
+	public static void main(String[] args) {
+
+
+		TestEu1 obj = new TestEu1(2);
+		obj.setVarsta(5);
+		System.out.println(obj.varsta);
+
+	}
+
+}
